1beaf0a2338884060edd139edaa95243c4376fd8
BColantonio <BColantonio@email.neit.edu>
2018-10-20 02:04:11 -0400

    first commit

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language = "C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="wk2InClassAssignment.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns = "http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id = "form1" runat="server">
+        <div>
+            <asp:Button ID="btnContact" runat="server" OnClick="btnContact_Click" Text="Display Contact Info." />
+            <asp:Button ID="btnMen" runat="server" OnClick="btnMen_Click" Text="Men's Info" />
+            <asp:Button ID="btnLast" runat="server" OnClick="btnLast_Click" Text="Search Last Name" />
+            <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+            <asp:Button ID = "btnVersion" runat="server" OnClick="btnVersion_Click" Text="Display Version Info." />
+            <asp:TextBox ID = "txtResult" runat="server" Height="584px" Width="558px" ReadOnly="True" TextMode="MultiLine"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace wk2InClassAssignment
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnVersion_Click(object sender, EventArgs e)
+		{
+			XPathNavigator nav;
+			XPathDocument docNav;
+			XPathNodeIterator NodeIter;
+
+			String rootPath = Server.MapPath("~");
+			string strFilename = rootPath + "\\ContactManager.xml";
+			docNav = new XPathDocument(strFilename);
+
+			// Create a navigator to query with XPath.
+			nav = docNav.CreateNavigator();
+			String searchString = "//VersionDate | //VersionNumber";
+
+			//Select the node and place the results in an iterator.
+			NodeIter = nav.Select(searchString);
+			nav = NodeIter.Current;
+			while (NodeIter.MoveNext())
+			{
+				txtResult.Text += "\n\nVersion Date = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nVersion Number = " + nav.Value;
+
+			}
+
+
+
+		}
+
+		protected void btnContact_Click(object sender, EventArgs e)
+		{
+			
+			XPathNavigator nav;
+			XPathDocument docNav;
+			XPathNodeIterator NodeIter;
+
+			String rootPath = Server.MapPath("~");
+			string strFilename = rootPath + "\\ContactManager.xml";
+			docNav = new XPathDocument(strFilename);
+
+			// Create a navigator to query with XPath.
+			nav = docNav.CreateNavigator();
+			String searchString = "//@Sex | //FirstName | //LastName | //PhoneNumber | //FaxNumber | //EmailAddress";
+
+			//Select the node and place the results in an iterator.
+			NodeIter = nav.Select(searchString);
+			nav = NodeIter.Current;
+			while (NodeIter.MoveNext())
+			{
+				txtResult.Text += "\n\nGender = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nFirst Name = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nLast Name = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nPhone Number = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nFax Number = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nEmail = " + nav.Value;
+
+			}
+
+			
+
+
+		}
+
+		protected void btnMen_Click(object sender, EventArgs e)
+		{
+			XPathNavigator nav;
+			XPathDocument docNav;
+			XPathNodeIterator NodeIter;
+
+			String rootPath = Server.MapPath("~");
+			string strFilename = rootPath + "\\ContactManager.xml";
+			docNav = new XPathDocument(strFilename);
+
+			// Create a navigator to query with XPath.
+			nav = docNav.CreateNavigator();
+			String searchString = "//Contact[@Sex = 'M']/FirstName | //Contact[@Sex = 'M']/LastName | //Contact[@Sex = 'M']/PhoneNumber | //Contact[@Sex = 'M']/FaxNumber | //Contact[@Sex = 'M']/EmailAddress";
+
+			//Select the node and place the results in an iterator.
+			NodeIter = nav.Select(searchString);
+			nav = NodeIter.Current;
+			txtResult.Text += "\n\nMen's Contact Info:";
+			while (NodeIter.MoveNext())
+			{
+				txtResult.Text += "\nFirst Name = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nLast Name = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nPhone Number = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nFax Number = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nEmail = " + nav.Value;
+
+			}
+		}
+
+		protected void btnLast_Click(object sender, EventArgs e)
+		{
+			XPathNavigator nav;
+			XPathDocument docNav;
+			XPathNodeIterator NodeIter;
+
+			String rootPath = Server.MapPath("~");
+			string strFilename = rootPath + "\\ContactManager.xml";
+			docNav = new XPathDocument(strFilename);
+
+			// Create a navigator to query with XPath.
+			nav = docNav.CreateNavigator();
+			String search = txtSearch.Text;
+			String searchString = "//Contact[LastName = '"+ search +"']/./@Sex | //Contact[LastName = '" + search + "']/FirstName | //Contact[LastName = '" + search + "']/LastName | //Contact[LastName = '" + search + "']/PhoneNumber |//Contact[LastName = '" + search + "']/FaxNumber | //Contact[LastName = '" + search + "']/EmailAddress";
+
+			//Select the node and place the results in an iterator.
+			NodeIter = nav.Select(searchString);
+			nav = NodeIter.Current;
+			txtResult.Text += "\n\n"+ search +" Contact Info:";
+			while (NodeIter.MoveNext())
+			{
+				txtResult.Text += "\nGender = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nFirst Name = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nLast Name = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nPhone Number = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nFax Number = " + nav.Value;
+				NodeIter.MoveNext();
+				txtResult.Text += "\nEmail = " + nav.Value;
+
+			}
+		}
+	}
+}
